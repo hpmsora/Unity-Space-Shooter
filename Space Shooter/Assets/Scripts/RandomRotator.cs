4c3da00a577f548eec978637501d992617d81ae3
Won Yong Ha <hpmsora@gmail.com>
2017-03-12 20:14:50 -0400

    12 March 2017

---
+++ Space Shooter/Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour {
+	public float tumnble;
+
+	private Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+
+		rb.angularVelocity = Random.insideUnitSphere * tumnble;
+	}
+}
